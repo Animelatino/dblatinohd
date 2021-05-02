9c6816646e8522fb30bcc4ff7283f2c5568d763d
AnimeLatino <malcarichard6@gmail.com>
2021-05-02 12:57:56 -0500

    fixed Chat All Pages

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,9 @@
 import React, { PureComponent } from 'react';
-
+import { Head } from 'next/document';
 import NavNavigation from './NavNavigation';
 
 import styles from '../styles/Layout.module.css';
+
 
 class Layout extends PureComponent {
 
@@ -13,6 +14,13 @@ class Layout extends PureComponent {
     render() {
         return (
             <>
+                <Head>
+                    <link rel="manifest" href="/manifest.json"/>
+                    <meta name="robots" content="index, follow"/>
+                    <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}/>
+                    <script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${GA_TRACKING_ID}', {page_path: window.location.pathname,});`,}}/>
+                    <script id="chatBroEmbedCode" src="/js/chat.js"></script>
+                </Head>
                 <NavNavigation/>
                 <div className={styles.container}>
                     {this.props.children}
