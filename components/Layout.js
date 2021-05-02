000cc2131b45238a1df925e4db2c7171b5881518
AnimeLatino <malcarichard6@gmail.com>
2021-05-02 13:03:11 -0500

    fixed Bugs

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,4 @@
 import React, { PureComponent } from 'react';
-import { Head } from 'next/document';
 import NavNavigation from './NavNavigation';
 
 import styles from '../styles/Layout.module.css';
@@ -14,17 +13,13 @@ class Layout extends PureComponent {
     render() {
         return (
             <>
-                <Head>
-                    <link rel="manifest" href="/manifest.json"/>
-                    <meta name="robots" content="index, follow"/>
-                    <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}/>
-                    <script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${GA_TRACKING_ID}', {page_path: window.location.pathname,});`,}}/>
-                    <script id="chatBroEmbedCode" src="/js/chat.js"></script>
-                </Head>
                 <NavNavigation/>
                 <div className={styles.container}>
                     {this.props.children}
                 </div>
+                <footer>
+                    <script id="chatBroEmbedCode" src="/js/chat.js"></script>
+                </footer>
             </>
         )
     }
--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,10 @@ class MyDocument extends Document {
                 <Head>
                     <link rel="icon" href="/favicon.ico"/>
                     <meta name="theme-color" content="#000000"/>
+                    <link rel="manifest" href="/manifest.json"/>
+                    <meta name="robots" content="index, follow"/>
+                    <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}/>
+                    <script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${GA_TRACKING_ID}', {page_path: window.location.pathname,});`,}}/>
                 </Head>
                 <body>
                     <Main />
