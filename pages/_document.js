ab139ff50aa01ea93086ca4f1b026314175dd12e
AnimeLatino <malcarichard6@gmail.com>
2021-05-02 14:05:57 -0500

    add arc.io

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ class MyDocument extends Document {
                     <meta name="robots" content="index, follow"/>
                     <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}/>
                     <script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${GA_TRACKING_ID}', {page_path: window.location.pathname,});`,}}/>
+                    <script async src="https://arc.io/widget.min.js#5NgTk75z"></script>
                 </Head>
                 <body>
                     <Main />
