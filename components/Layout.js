d928df4f9b28d8357c6b602e370df1d493eea142
AnimeLatino <malcarichard6@gmail.com>
2021-05-02 14:03:08 -0500

    Add BottomNavigation

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,9 @@
 import React, { PureComponent } from 'react';
+
 import NavNavigation from './NavNavigation';
+import BottomNavigation from './BottomNavigation';
 
 import styles from '../styles/Layout.module.css';
-
 
 class Layout extends PureComponent {
 
@@ -17,6 +18,7 @@ class Layout extends PureComponent {
                 <div className={styles.container}>
                     {this.props.children}
                 </div>
+                <BottomNavigation/>
                 <footer>
                     <script id="chatBroEmbedCode" src="/js/chat.js"></script>
                 </footer>
